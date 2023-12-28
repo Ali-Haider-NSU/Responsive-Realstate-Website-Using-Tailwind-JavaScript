dde9e84e545df08e041324c5e866c24dafa076cd
Ali-Haider-NSU <mdalihaider800@gmail.com>
2023-12-28 22:56:39 +0600

    complete property type section

--- script.js
+++ script.js
@@ -10,68 +10,75 @@ function onToggleMenu(e) {
     }
 
 }
+// window.addEventListener("resize", function(event) {
+//     location.reload();
+//     event.preventDefault();
 
+// });
 const property = document.querySelector('.property');
 const subproperty = document.querySelector('.subproperty');
 const pages = document.querySelector('.pages');
 const subpages = document.querySelector('.subpages');
 
-if (window.innerWidth <= 1024) {
-    resizedWindow();
-}
+// if (window.innerWidth <= 1000) {
+//     resizedWindow();
+// }
 
-if (window.innerWidth > 1024) {
-    resizedWindow();
-}
+// if (window.innerWidth > 1000) {
+//     resizedWindow();
+//     // subproperty.style.display = 'none'
+// }
 
 function resizedWindow() {
-    //location.reload();
-    //window.onload();
-    //console.log(innerWidth)
-    if (window.innerWidth <= 1024) {
 
+    if (window.innerWidth <= 1000) {
+        // subproperty.style.display = 'none'
+        // subpages.style.display = 'none'
         property.addEventListener('click', () => {
-            console.log(innerWidth)
+
             if (subproperty.style.display === 'none' || subproperty.style.display === '') {
                 subproperty.style.display = 'block'
-                    // subproperty.name = 'chevron-up-outline'
-                    // subproperty.name === 'chevron-down-outline' ? 'chevron-up-outline' : 'chevron-down-outline'
+
             } else {
                 subproperty.style.display = 'none'
-                    // subproperty.name = subproperty.name === 'chevron-up-outline' ? 'chevron-down-outline' : 'chevron-up-outline'
+
             }
         })
         pages.addEventListener('click', () => {
             if (subpages.style.display === 'none' || subpages.style.display === '') {
                 subpages.style.display = 'block'
-                    // subproperty.name = 'chevron-up-outline'
-                    // subproperty.name === 'chevron-down-outline' ? 'chevron-up-outline' : 'chevron-down-outline'
+
             } else {
                 subpages.style.display = 'none'
-                    // subproperty.name = subproperty.name === 'chevron-up-outline' ? 'chevron-down-outline' : 'chevron-up-outline'
+
             }
         })
-    } else if (window.innerWidth > 1024) {
-        property.addEventListener('mouseover', () => {
-            subproperty.style.position = 'absolute'
-            subproperty.style.display = 'block'
-        })
-        property.addEventListener('mouseout', () => {
-            subproperty.style.position = 'absolute'
-            subproperty.style.display = 'none'
-        })
-        pages.addEventListener('mouseover', () => {
-            subpages.style.position = 'absolute'
-            subpages.style.display = 'block'
-        })
-        pages.addEventListener('mouseout', () => {
-            subpages.style.position = 'absolute'
-            subpages.style.display = 'none'
-        })
+    } else {
+        // subproperty.style.display = 'none'
+        // subpages.style.display = 'none'
+        // property.addEventListener('mouseover', () => {
+        //     subproperty.style.position = 'absolute'
+        //     subproperty.style.display = 'block'
+        // })
+        // property.addEventListener('mouseout', () => {
+        //     subproperty.style.position = 'absolute'
+        //     subproperty.style.display = 'none'
+        // })
+        // pages.addEventListener('mouseover', () => {
+        //     subpages.style.position = 'absolute'
+        //     subpages.style.display = 'block'
+        // })
+        // pages.addEventListener('mouseout', () => {
+        //     subpages.style.position = 'absolute'
+        //     subpages.style.display = 'none'
+        // })
     }
 }
 
 window.addEventListener('resize', resizedWindow)
+
+
+
 
 
 // Changing Slide Carousel
